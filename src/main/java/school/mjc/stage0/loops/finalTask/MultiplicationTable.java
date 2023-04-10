8c624738a593e71aba8dcd31f14816d21d95673e
Meerim Sadykbekova <sadykbekova.m@gmail.com>
2023-04-10 18:53:11 +0600

    Completed all tasks.

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -1,6 +1,14 @@
 package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
-    public void printTable(int numberTableToPrint){
+    public static void printTable(int numberTableToPrint){
+        for (int i = 1; i <= 10; i++) {
+            int result = numberTableToPrint * i;
+            System.out.println(i + " x " + numberTableToPrint + " = " + result);
+        }
+    }
+
+    public static void main(String[] args) {
+        printTable(3);
     }
 }
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,19 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            int k = 1;
+            for (int j = i; j < cathetusLength; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j > 1; j--) {
+                System.out.print(j);
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(k++);
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
